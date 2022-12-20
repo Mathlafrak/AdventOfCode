25c17f6e8e9c76e5b27407349693c008c090619f
Mathis Bui Khac <mathis.bui-khac@sagemcom.com>
2022-12-20 16:47:14 +0100

    Update

--- AdventofCode2022/Day3/AdventofCode2022_Day3_part2.py
+++ AdventofCode2022/Day3/AdventofCode2022_Day3_part2.py
@@ -1,10 +1,10 @@
 import string
-data = open("day03_2022.txt", 'r')
+data = open("/home/g700830/Workspace/AdventofCode/AdventofCode2022/Day3/day03_2022.txt", 'r')
 input = []
 group = []
 for lines in data:
+    lines = lines.replace('\n', '')
     if len(group) < 3:
-        lines = lines.replace('\n', '')
         group.append(lines)
         if len(input) == 99:
             input.append(group)
@@ -16,28 +16,32 @@ for lines in data:
 print(input)
 input = [set(s) for s in input]
 print(input)
-def GetPriority(list):
+def GetPriority(liste):
+    global commonLetters
+    commonLetters = []
+    for element in liste:
+        common = set.intersection(*map(set,element)) #Get the common character between the 3 strings of the set "element"
+        commonLetters.append(list(common))
+    
+    print(commonLetters)
+    return GetValue(commonLetters)
+
+    # return compteur
+
+
+
+def GetValue(common_letter):
     global compteur
-    global priority
-    letter = []
+    compteur = 0
+    alphabet = string.ascii_lowercase
+    ALPHABET = string.ascii_uppercase
+    for letters in range(len(common_letter)):
+        if common_letter[letters][0] in ALPHABET:
+            compteur += 27 + ALPHABET.index(common_letter[letters][0])
+        else:
+            compteur += alphabet.index(common_letter[letters][0]) + 1
+    return compteur
 
-    common = set.intersection(*map(set, list[0]))
-   
-    print(common)
+print(GetPriority(input))
 
-GetPriority(input)
-    # if priority in ALPHABET:
-    #     compteur += 27 + ALPHABET.index(priority)
-    # else:
-    #     compteur += alphabet.index(priority) + 1
-
-
-# alphabet = string.ascii_lowercase
-# ALPHABET = string.ascii_uppercase
-# # print(alphabet)
-# # print(ALPHABET)
-#
-# for element in range(len(input)):
-#     GetPriority(input[element])
-# # print(compteur)
 
---
+++ AdventofCode2022/Day4/AdventofCode2022_day4_part1.py
@@ -0,0 +1,32 @@
+import re
+data = open("/home/g700830/Workspace/AdventofCode/AdventofCode2022/Day4/day04_2022.txt",'r')
+
+input = []
+for lines in data:
+
+    lines = lines.replace("\n", "")
+
+    Split = re.split(r'-|,', lines)
+    input.append(Split)
+
+transit = []
+for lines in input:   
+    moove = []
+    for character in lines:
+        if character.isdigit():
+            moove.append(int(character))    
+    transit.append(moove)
+compteur = 0 
+
+for mooves in transit:
+    a = mooves[0]
+    b = mooves[1]
+    c = mooves[2]
+    d = mooves[3]
+    if a >= c and b <= d or a <= c and b >= d:
+        compteur += 1  
+
+
+
+
+print(compteur)
